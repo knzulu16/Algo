bb159b40dac144af09a0bbbb56b3dea6136e8814
Nonzulu <knzulu16@gmail.com>
2017-05-10 16:20:47 +0200

    save

--- algo.js
+++ algo.js
@@ -1,26 +1,27 @@
-var string="1 for R3, 2 for R7, 3 for R10, 5 for R14.50.";
+var string="1 for R3, 2 for R7, 3 for R10, 5 for R14.50";
 var avo=0;
 function deal(string){
-var avo=string.split(",");
-console.log(avo);
+  var avo=string.split(", ");
+  console.log(avo);
+  return avo;
 }
 deal(string);
 
 
-// function avoPrice(priceS){
-//   var price=priceS.split(',');
-//   var quant=Number(Price[0]);
-//   console.log(quant);
-//   return price;
-// }
-//
-//
-//
-// function calc(each){
-// var list=[];
-// for(var i=0;i<string.length;i++){
-//   var arra=string[i];
-//
-// }
-// console.log(list);
-// }
+function avoPrice(priceS){
+  var price=priceS.split(',');
+  var quant=Number(Price[0]);
+  console.log(quant);
+  return priceS;
+}
+
+
+
+function calc(each){
+var list=[];
+for(var i=0;i<string.length;i++){
+  var arra=string[i];
+
+}
+console.log(list);
+}
--- tests/tests.algo.js
+++ tests/tests.algo.js
@@ -1,7 +1,10 @@
-var string=("1 for R3, 2 for R7, 3 for R10, 5 for R14.50.");
+var string="1 for R3, 2 for R7, 3 for R10, 5 for R14.50.".split(', ');
 describe('deal()', function(){
   it('Should display the splitted list',function(){
-  assert.equal(deal("string"),("1 for R3","2 for R7","3 for R10","5 for R14.50"));
+  assert.deepEqual(deal(string),["1 for R3","2 for R7","3 for R10","5 for R14.50"]);
 
 });
+});
+it('Should display the length of the list',function(){
+assert.equal(deal("string"),4);
     });
